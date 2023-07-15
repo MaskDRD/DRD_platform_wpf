dbf7e624225c0cfe4fcf04dd06235e8a20b70842
Maksim <email@mail.ru>
2023-07-15 16:44:27 +0400

    pf-1 tlicroservices (класс ingleton)

---
+++ UtlMicroservices/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace platform.UtlMicroservices
+{
+    /// <summary>
+    /// Класс создающий паттерн Singleton (для service, и др. объектов который могут сущестововать в 1 экземпляре.
+    /// </summary>
+    public class Singleton<T> where T : new()
+    {
+        private static T _instance;
+        /// <summary>
+        ///  Объект для обращения к единому экземпляру класса.
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+                return _instance;
+            }
+        }
+    }
+}
