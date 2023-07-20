50e89d4fcad7b70934e977a8bf5425985f18263a
Maksim <email@mail.ru>
2023-07-20 18:11:54 +0400

    подключение окна вход в систему в главному окну

--- AuthMicroservices/view/AuthorizationWindow.xaml.cs
+++ AuthMicroservices/view/AuthorizationWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace platform.AuthMicroservices.view
@@ -18,7 +17,7 @@ namespace platform.AuthMicroservices.view
     /// <summary>
     /// Логика взаимодействия для AuthorizationWindow.xaml
     /// </summary>
-    public partial class AuthorizationWindow : Page
+    public partial class AuthorizationWindow : Window
     {
         public AuthorizationWindow()
         {
