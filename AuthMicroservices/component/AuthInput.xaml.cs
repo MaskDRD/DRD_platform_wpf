5d4e7de74ecfe5fea3390b6647fc168ef248fd26
Maksim <email@mail.ru>
2023-07-22 17:45:28 +0400

    PF-2 AuthMicroservices - Базовые компоненты пере используемые

---
+++ AuthMicroservices/component/AuthInput.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace platform.AuthMicroservices.component
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthInput.xaml
+    /// </summary>
+    public partial class AuthInput : UserControl
+    {
+        /// <summary>
+        /// DependencyProperty зависимость текстовка у label
+        /// </summary>
+        public static readonly DependencyProperty LabelTextProperty = DependencyProperty.Register(
+            "LabelText",
+            typeof(string),
+            typeof(AuthInput)
+        );
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        public string value
+        {
+            get { return input.Text; }
+        }
+
+        /// <summary>
+        /// Функция клик на label перенос фокуса на элемент
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LabelMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Label label = (Label)sender;
+            if (e.ClickCount == 1 && label.Target != null)
+            {
+                Keyboard.Focus(label.Target);
+            }
+        }
+
+        public AuthInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AuthMicroservices/component/AuthTitle.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace platform.AuthMicroservices.component
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthTitle.xaml
+    /// </summary>
+    public partial class AuthTitle : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            "Title",
+            typeof(string),
+            typeof(AuthTitle)
+        );
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public AuthTitle()
+        {
+            InitializeComponent();
+        }
+    }
+}
