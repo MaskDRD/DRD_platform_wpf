5f02101ae035fd9efdf98c695ee461a79d3b2adf
Maksim <email@mail.ru>
2023-07-22 09:21:57 +0400

    PF-2 AuthMicroservices - Окно входа

--- AuthMicroservices/view/AuthorizationWindow.xaml.cs
+++ AuthMicroservices/view/AuthorizationWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-
+using System.Windows.Media;
 
 namespace platform.AuthMicroservices.view
 {
@@ -27,7 +27,24 @@ namespace platform.AuthMicroservices.view
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
+            if(InputLogin.Text == "")
+            {
+                TextErrorNullLogin.Visibility = Visibility.Visible;
+            }
 
+            if(InputPassword.Password == "") { 
+                TextErrorNullPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void InputLoginFocusableChanged(object sender, TextChangedEventArgs e)
+        {
+            TextErrorNullLogin.Visibility = Visibility.Collapsed;
+        }
+
+        private void InputPasswordPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            TextErrorNullPassword.Visibility = Visibility.Collapsed;
         }
     }
 }
