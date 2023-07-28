68ebf80efae2e5ed226849a72d0585f53bd19368
Maksim <email@mail.ru>
2023-07-28 14:58:33 +0400

    PF-6 BdMicroservices - 1 версия подключение mysql бд

---
+++ BdMicroservices/Cache/BdConnectCache.cs
@@ -0,0 +1,23 @@
+﻿using MySqlConnector;
+using platform.UtlMicroservices;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace platform.BdMicroservices.Cache
+{
+    sealed class BdConnectCache: Singleton<BdConnectCache>
+    {
+        public Dictionary<string, MySqlConnection> connectBd = new Dictionary<string, MySqlConnection>();
+        public BdConnectCache()
+        {
+            ConnectionStringSettingsCollection connectionStringSettingsCollection = ConfigurationManager.ConnectionStrings;
+            foreach (ConnectionStringSettings connectionStringSettings in connectionStringSettingsCollection)
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection(connectionStringSettings.ConnectionString);
+                connectBd.Add(connectionStringSettings.Name, mySqlConnection);
+            }
+        }
+    }
+}
