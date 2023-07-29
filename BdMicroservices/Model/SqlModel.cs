54501983e43086a37c12aa036f9270a17ff8234e
Maksim <email@mail.ru>
2023-07-29 11:16:06 +0400

    PF-6 BdMicroservices - Model

---
+++ BdMicroservices/Model/SqlModel.cs
@@ -0,0 +1,42 @@
+﻿namespace platform.BdMicroservices.Model
+{
+    class SqlModel
+    {
+        public SqlModel(string sql, string connectionName) { 
+            ConnectionName = connectionName;
+            Sql = sql;
+        }
+
+        public SqlModel(
+            string sql,
+            string connectionName,
+            SqlParamsModel paramsIn
+        ) {
+            ConnectionName = connectionName;
+            Sql = sql;
+            ParamsIn = paramsIn;
+        }
+
+        public SqlModel(
+            string sql,
+            string connectionName,
+            SqlParamsModel paramsIn,
+            SqlParamsModel paramsOut
+        ) {
+            ConnectionName = connectionName;
+            Sql = sql;
+            ParamsIn = paramsIn;
+            ParamsOut = paramsOut;
+        }
+
+
+        public string Type { get; set; }
+
+        public string Sql { get; set; }
+
+        public string ConnectionName { get; set; }
+
+        public SqlParamsModel ParamsIn { get; set; }
+        public SqlParamsModel ParamsOut { get; set; }
+    }
+}
---
+++ BdMicroservices/Model/SqlParamsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace platform.BdMicroservices.Model
+{
+    class SqlParamsModel
+    {
+        public SqlParamsModel(string name) {
+            Name = name;
+        } 
+
+        public string Name { get; set; }
+    }
+}
