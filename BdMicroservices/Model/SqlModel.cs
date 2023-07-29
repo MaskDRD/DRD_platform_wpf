4635bfed2c1f2ef0f16bf52207a01b3793bf9333
Maksim <email@mail.ru>
2023-07-29 11:24:41 +0400

    PF-6 BdMicroservices - Model

--- BdMicroservices/Model/SqlModel.cs
+++ BdMicroservices/Model/SqlModel.cs
@@ -2,39 +2,29 @@
 {
     class SqlModel
     {
-        public SqlModel(string sql, string connectionName) { 
-            ConnectionName = connectionName;
+        public SqlModel(string sql) { 
             Sql = sql;
         }
 
         public SqlModel(
             string sql,
-            string connectionName,
             SqlParamsModel paramsIn
         ) {
-            ConnectionName = connectionName;
             Sql = sql;
             ParamsIn = paramsIn;
         }
 
         public SqlModel(
             string sql,
-            string connectionName,
             SqlParamsModel paramsIn,
             SqlParamsModel paramsOut
         ) {
-            ConnectionName = connectionName;
             Sql = sql;
             ParamsIn = paramsIn;
             ParamsOut = paramsOut;
         }
 
-
-        public string Type { get; set; }
-
         public string Sql { get; set; }
-
-        public string ConnectionName { get; set; }
 
         public SqlParamsModel ParamsIn { get; set; }
         public SqlParamsModel ParamsOut { get; set; }
