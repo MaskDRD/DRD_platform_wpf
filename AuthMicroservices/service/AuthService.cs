aefb4930bb1517527aaf624f620997ed8bf4d764
Maksim <email@mail.ru>
2023-08-01 15:43:52 +0400

    PF-2 AuthMicroservices - AuthServer

---
+++ AuthMicroservices/service/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using platform.UtlMicroservices;
+using System;
+using System.Security.Cryptography;
+
+namespace platform.AuthMicroservices.service
+{
+    sealed class AuthService: Singleton<AuthService>
+    {
+        private string HashPassword(string password)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] hash = new SHA256CryptoServiceProvider().ComputeHash(data);
+            return Convert.ToBase64String(hash);
+        }
+        private bool CheckPassword(string password, string password_check)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] hash = new SHA256CryptoServiceProvider().ComputeHash(data);
+            return Convert.ToBase64String(hash).Equals(password_check);
+        }
+        private string GenerateToken(int userId)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today.ToString());
+            string token = today.ToString() + userId.ToString();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(token);
+            byte[] hash = new SHA256CryptoServiceProvider().ComputeHash(data);
+            return Convert.ToBase64String(hash);
+        }
+
+        public void register() { }
+        public void authorization() { }
+        public void authentication() { }
+    }
+}
