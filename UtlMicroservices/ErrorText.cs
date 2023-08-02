8cad729cfc4ee79f94bbbb4fb10e26248a47795e
Maksim <email@mail.ru>
2023-08-02 20:26:55 +0400

    PF-1 UtlMicroservices - ErrorText

---
+++ UtlMicroservices/ErrorText.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+
+namespace platform.UtlMicroservices
+{
+    public class ErrorText
+    {
+        public static string UserNikUnique(string nik) {
+            return "Ник" + nik + "уже существует";
+        }
+
+        public static string UserNikUnique()
+        {
+            return "Ник уже существует";
+        }
+        public static string UserLoginUnique()
+        {
+            return "Логин уже существует";
+        }
+        public static string UserEmailUnique()
+        {
+            return "емайл уже существует";
+        }
+    }
+}
